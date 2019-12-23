2ac5c35486e141b62351b295dd460479817792f4
alejsherion <alejandro5504@gmail.com>
2019-12-22 19:25:54 -0500

    Generating solution chat, and implement SignalR

---
+++ ChatChallenge/WebAppChat/Pages/Chatpage.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppChat
+{
+    public class ChatpageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ ChatChallenge/WebAppChat/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppChat.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (User.Identity.Name != null)
+            {
+                ViewData["IsLogin"] = true;
+                ViewData.Add("UserName", User.Identity.Name);
+            }
+        }
+    }
+}
